4a53bc3176abca79478ee43d821153dfcc24d1d9
forjing <xiaodoubao1994@gmail.com>
2019-01-30 22:48:03 +0800

    add selectionSort

---
+++ selection_sort.js
@@ -0,0 +1,29 @@
+/**
+ *  选择排序
+ *  每次找出一个最小的元素， 放入新数组
+ *
+ */
+
+function findSmallest(arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+  arr.forEach((item, index) => {
+    if (item < smallest) {
+      smallest = item;
+      smallestIndex = index;
+    }
+  });
+  return smallestIndex;
+}
+
+function selectionSort(arr) {
+  let newArr = [];
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let smallestIndex = findSmallest(arr);
+    newArr = newArr.concat(arr.splice(smallestIndex, 1));
+    console.log("arr:", arr);
+    console.log("newArr:", newArr);
+  }
+  return newArr;
+}
