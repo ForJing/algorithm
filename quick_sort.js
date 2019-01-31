2507e0633008c8cadec31d9e14b4c8f53b63f944
forjing <xiaodoubao1994@gmail.com>
2019-01-31 11:19:24 +0800

    add quick sort

---
+++ quick_sort.js
@@ -0,0 +1,20 @@
+/**
+ *
+ *  快速排序
+ *
+ *  一个数组，
+ *  找一个基准值， 比这个值小的放左边， 比这个值大的放右边。
+ *
+ *
+ */
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  } else {
+    let pivot = arr[0];
+    let less = arr.slice(1).filter(item => item <= pivot);
+    let greater = arr.slice(1).filter(item => item > pivot);
+    return [...less, pivot, ...greater];
+  }
+}
